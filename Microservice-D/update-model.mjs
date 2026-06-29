8636e277b0a82253a3a78bc8b3870ba6caace9a5
gracemeyer2015 <meyerg3@oregonstate.edu>
2026-06-29 15:42:24 -0700

    fixed model setup

--- Microservice-D/update-model.mjs
+++ Microservice-D/update-model.mjs
@@ -1,7 +1,38 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-import {Artist, connect} from '../cs-361-music-artist-app-copy/music-rest/music-model.mjs';
+
+
+const MUSIC_DB_NAME = 'Artist'
+
+export async function connect(){
+    try{
+       await mongoose.connect(process.env.MONGODB_CONNECT_STRING, 
+                {dbName: MUSIC_DB_NAME});
+        console.log("Successfully connected to MongoDB using Mongoose!");
+    } catch(err){
+        console.log(err);
+        throw Error(`Could not connect to MongoDB ${err.message}`)
+    }
+}
+
+const artistSchema = mongoose.Schema({
+
+    name:{type: String, required: true},
+    bio:{type: String, required: true},
+    genre:{type: String, required: true},
+   imageBase64:{
+        type: String
+},
+    
+
+})
+
+
+/**
+ * Compile the model from the schema 
+ */
+export const Artist = mongoose.model(MUSIC_DB_NAME, artistSchema)
 
 await connect();
 
